f2a6ca1307768aca7e0e34f37d38eb74ee56ae5e
Facu <cayofacundo1@gmail.com>
2026-09-03 04:57:27 -0300

    Added compose navigation as dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,6 +51,7 @@ dependencies {
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
     implementation(libs.kotlin.serialization.json)
+    implementation(libs.androidx.navigation.compose)
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
